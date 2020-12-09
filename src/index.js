a55221008267f44e21175bb8cbbb828cdf987daf
adelia ferreira <tay.figueira@gmail.com>
2020-12-09 14:59:38 +0000

    Adding lint, yargs and configure these packages
    Signed-off-by: adelia ferreira <tay.figueira@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('testando');
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+
+const options = yargs
+  .usage('Usage: -lat <latitude>')
+  .option('lat', {
+    alias: 'latitude', describe: 'office latitude', type: 'string', demandOption: false,
+  })
+  .usage('Usage: -long <longitude>')
+  .option('long', {
+    alias: 'longitude', describe: 'office longitude', type: 'string', demandOption: false,
+  })
+  .usage('Usage: -fp <path>')
+  .option('fp', {
+    alias: 'path', describe: 'file path', type: 'string', demandOption: false,
+  })
+  .argv;
+
+const greeting = `invite-client, ${options.lat}! ${options.long}! ${options.fp}!`;
+
+console.log(greeting);
