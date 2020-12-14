63a9ee44ba730defccf5491ae2a4270b551e81c6
adelia ferreira <tay.figueira@gmail.com>
2020-12-14 00:34:15 +0000

    Adding read and write files
    Signed-off-by: adelia ferreira <tay.figueira@gmail.com>

--- src/files-utils/read-file.js
+++ src/files-utils/read-file.js
@@ -5,8 +5,12 @@ const constValue = require('./constant');
 exports.getCustomerList = (
   filepath = path.join(__dirname, constValue.INPUT_CUSTOMERS_FILE_PATH),
 ) => {
-  const contents = fs.readFileSync(filepath, 'utf8');
-  const jsonStringContent = contents.split('\n');
-  const clientInformation = jsonStringContent.map((clientInfoJson) => JSON.parse(clientInfoJson));
-  return clientInformation;
+  try {
+    const contents = fs.readFileSync(filepath, 'utf8');
+    const jsonStringContent = contents.split('\n');
+    const clientInformation = jsonStringContent.map((clientInfoJson) => JSON.parse(clientInfoJson));
+    return clientInformation;
+  } catch (error) {
+    throw new Error('Cannot retrive file from dir selected');
+  }
 };
--- src/files-utils/write-file.js
+++ src/files-utils/write-file.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const constValue = require('./constant');
 
-exports.writeOutputFile = (filepath = constValue.OUTPUT_CUSTOMERS_IN_RANGE) => [];
+exports.writeOutputFile = (
+  clientToInvite, filepath = path.join(__dirname, constValue.OUTPUT_CUSTOMERS_IN_RANGE),
+) => {
+  let fileContent = '';
+  clientToInvite.forEach((client) => {
+    fileContent += `${JSON.stringify(client)}\n`;
+  });
+  fs.writeFileSync(filepath, fileContent);
+  // eslint-disable-next-line no-console
+  console.log(`Your client list to invite is also available on ${filepath}`);
+};
